e95a66b82b2fae54180e8ea0c297af16fd355f88
Piyush Jain <piyushjainwork01@gmail.com>
2025-11-16 04:07:12 +0530

    Setup complete: Templates working, IntelliJ configured

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Setup successful!");
+
+        // Test template import
+        templates.prefixsum.PrefixSumCount template =
+                new templates.prefixsum.PrefixSumCount();
+
+        int[] nums = {1, 1, 1};
+        int result = template.subarraySum(nums, 2);
+        System.out.println("Result: " + result);  // Should print 2
+    }
 }
