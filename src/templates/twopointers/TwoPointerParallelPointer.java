59468c914f60b5c5b34b6c99dcad00fd3175a4d7
Piyush Jain <piyushjainwork01@gmail.com>
2025-11-18 21:52:18 +0530

    added new algo pattern for Two pointer approach

---
+++ src/templates/twopointers/TwoPointerParallelPointer.java
@@ -0,0 +1,10 @@
+package templates.twopointers;
+
+public class TwoPointerParallelPointer {
+
+
+    /* TODO:- Given an integer array nums and an integer val, remove all occurrences of val
+        in nums in-place. Return the number of elements in nums which are not equal to val.*/
+
+    
+}
